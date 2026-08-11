cfd83f453eb5c8ac692cb3aecd081adb9bb0e270
iamcarlos3110 <iamcarlos3110@gmail.com>
2026-08-11 18:57:30 +0200

    Fix Stripe Managed Payments parameter error

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -27,7 +27,6 @@ export default async function handler(req, res) {
 
     // Crear sesión de Stripe Checkout
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
       line_items: [
         {
           price: priceId,
