679ff3edacc8edd41e4053b0c4dff4b7a2b55351
iamcarlos3110 <iamcarlos3110@gmail.com>
2026-08-16 00:42:40 +0200

    Restore btnToggleFavorite to index.html and bump cache to v27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aprobados-ya-v26';
+const CACHE_NAME = 'aprobados-ya-v27';
 
 // Recursos estrictamente estáticos para arrancar la UI (solo rutas verificadas)
 const STATIC_ASSETS = [
