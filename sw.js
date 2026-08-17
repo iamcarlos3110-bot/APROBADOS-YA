de171acb416c99c208da55b09b1c2fe23f33a538
iamcarlos3110 <iamcarlos3110@gmail.com>
2026-08-17 19:01:55 +0200

    style: reorder navigation bar tabs as requested

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aprobados-ya-v46';
+const CACHE_NAME = 'aprobados-ya-v47';
 
 // Recursos estrictamente estáticos para arrancar la UI (solo rutas verificadas)
 const STATIC_ASSETS = [
